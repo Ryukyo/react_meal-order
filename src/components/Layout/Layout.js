f7ee768827918ac6a709c4048686280c14fb652e
Ryukyo <florian.fejer@hotmail.de>
2020-08-11 04:01:57 +0200

    Simple toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Aux from "../../utils/helper";
 import classes from "../../styles/Layout.css";
+import Toolbar from "../Navigation/Toolbar";
 
 const layout = (props) => (
   <Aux>
+    <Toolbar />
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>{props.children}</main>
   </Aux>
---
+++ src/components/Navigation/Toolbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "../../styles/Toolbar.css";
+
+const toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div>Logo</div>
+    <nav>...</nav>
+  </header>
+);
+
+export default toolbar;
